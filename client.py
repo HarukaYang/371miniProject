c082c5bfd3bcc5c0986417eeabe6bb67295b0984
dya41 <dya41@sfu.ca>
2022-11-25 14:29:07 -0800

    Added comments

--- client.py
+++ client.py
@@ -1,6 +1,5 @@
 import socket
 from datetime import datetime, timedelta
-import requests
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_address = ('localhost', 8000)
@@ -9,10 +8,11 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(server_address)
 request = 'GET /test.html HTTP/1.0\r\n'
 
-
+# Create header
 headers = 'Host: 0.0.0.0\r\n'
 
-# add more headers
+# Add more headers
+# Add if-modified-since header
 timestamp = datetime.utcnow() + timedelta(seconds=60)
 strTimestamp = timestamp.strftime('%a, %d %b %Y %H:%M:%S GMT')
 headers += 'if-modified-since: ' + strTimestamp + '\r\n'
